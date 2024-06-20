119d49ddd5a0ad85fd5aa73880306d0ad181fbb1
zero9852 <hihijkocg@163.com>
2024-06-21 00:58:54 +0800

    first commit

---
+++ demo0011/src/test/java/com/example/demo0011/Demo0011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo0011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
